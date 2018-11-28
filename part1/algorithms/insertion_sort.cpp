5683de363cc60593b2c10335fcb768abeb28a2b6
Christian Vaughn <cvaughn55@gmail.com>
2018-11-28 11:35:22 -0800

    add insertion sort

---
+++ part1/algorithms/insertion_sort.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+#include"../sort_algo.h"
+using namespace std;
+
+
+void insertionSort(vector<int> &list, SortAlgorithm::Compare cmp) {
+	int key;
+	int j;
+	for (size_t i = 1; i < list.size(); i++) {
+		key = list.at(i);
+		j = i-1;
+
+		while (j >=0 && cmp(list.at(j), key)) {
+			list.at(j+1) = list.at(j);
+			j--;
+		}
+		list.at(j+1) = key;
+	}
+}
+
+SortAlgorithm InsertionSort{"insertion sort", insertionSort};
