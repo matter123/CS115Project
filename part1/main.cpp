0e296c648b9301f5a5c337b0f734b87a14d03e3c
Matthew Fosdick <matthew.fosdick@gmail.com>
2018-11-28 10:01:50 -0800

    add explination of exponents

--- part1/main.cpp
+++ part1/main.cpp
@@ -1,21 +1,21 @@
 #include "test_driver.h"
 
 int main(int argc, char const *argv[]) {
-	TestDriver::testAlgorithms(0x0, 20);
-	TestDriver::testAlgorithms(0x1, 20);
-	TestDriver::testAlgorithms(0x2, 20);
-	TestDriver::testAlgorithms(0x3, 20);
-	TestDriver::testAlgorithms(0x4, 20);
-	TestDriver::testAlgorithms(0x5, 20);
-	TestDriver::testAlgorithms(0x6, 20);
-	TestDriver::testAlgorithms(0x7, 20);
-	TestDriver::testAlgorithms(0x8, 20);
-	TestDriver::testAlgorithms(0x9, 20);
-	TestDriver::testAlgorithms(0xA, 20);
-	TestDriver::testAlgorithms(0xB, 20);
-	TestDriver::testAlgorithms(0xC, 20);
-	TestDriver::testAlgorithms(0xD, 20);
-	TestDriver::testAlgorithms(0xE, 20);
-	TestDriver::testAlgorithms(0xF, 20);
+	TestDriver::testAlgorithms(0x0, 20); // N=2^0
+	TestDriver::testAlgorithms(0x1, 20); // N=2^1
+	TestDriver::testAlgorithms(0x2, 20); // N=2^2
+	TestDriver::testAlgorithms(0x3, 20); // N=2^3
+	TestDriver::testAlgorithms(0x4, 20); // N=2^4
+	TestDriver::testAlgorithms(0x5, 20); // N=2^5
+	TestDriver::testAlgorithms(0x6, 20); // N=2^6
+	TestDriver::testAlgorithms(0x7, 20); // N=2^7
+	TestDriver::testAlgorithms(0x8, 20); // N=2^8
+	TestDriver::testAlgorithms(0x9, 20); // N=2^9
+	TestDriver::testAlgorithms(0xA, 20); // N=2^10
+	TestDriver::testAlgorithms(0xB, 20); // N=2^11
+	TestDriver::testAlgorithms(0xC, 20); // N=2^12
+	TestDriver::testAlgorithms(0xD, 20); // N=2^13
+	TestDriver::testAlgorithms(0xE, 20); // N=2^14
+	TestDriver::testAlgorithms(0xF, 20); // N=2^15
 	return 0;
 }
