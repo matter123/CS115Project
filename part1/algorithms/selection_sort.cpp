475fe5bd9eed1866b2df5f29f2ca510e64e67f7b
Maheshwar Ravuri <mravuri96@gmail.com>
2018-11-28 11:57:44 -0800

    fixed a bug on selection_sort.cpp

--- part1/algorithms/selection_sort.cpp
+++ part1/algorithms/selection_sort.cpp
@@ -1,12 +1,13 @@
 #include <vector>
+#include <algorithm>
 #include "../sort_algo.h"
 using namespace std;
 
 void selectionSort(vector<int> &list, SortAlgorithm::Compare cmp) {
 	for(size_t i = 0; i < list.size(); i++) {
 		int temp2 = i;
-		for(size_t j = i; j < list.size(); j++) {
-			if(!cmp(list.at(j), list.at(i))) { temp2 = j; }
+		for(size_t j = i+1; j < list.size(); j++) {
+			if(cmp(list.at(j), list.at(temp2))) { temp2 = j; }
 		}
 		swap(list.at(i), list.at(temp2));
 	}
