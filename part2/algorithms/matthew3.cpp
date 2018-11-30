72e17f93becbd810bc169246f8f2f46195cbf5ff
Matthew Fosdick <matthew.fosdick@gmail.com>
2018-11-29 20:37:43 -0800

    add similar algorithm to matthew(2)

---
+++ part2/algorithms/matthew3.cpp
@@ -0,0 +1,26 @@
+#include <utility>
+#include "../in_set.h"
+bool matthew3(std::vector<int> &arr, int val, InSetAlgorithm::Compare cmp) {
+	using std::swap;
+	if(arr.size() < 2) { return false; }
+	auto mid = val / 2;
+
+	auto first = std::begin(arr);
+	auto partition = first;
+	auto last = std::end(arr);
+	// sort in ascending order
+	std::sort(first, last, [&](auto a, auto b) { return cmp(a, b) <= 0; });
+	--last;
+	while(first != last) {
+		auto compare = cmp(*first + *last, val);
+		if(compare == 0) { return true; }
+		if(compare > 0) {
+			--last;
+		} else {
+			++first;
+		}
+	}
+	return false;
+}
+
+InSetAlgorithm Matthew3{"matthew(3)", matthew3};
