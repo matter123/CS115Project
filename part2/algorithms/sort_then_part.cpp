f458f7b2c9f1ae5c36dd956a7c294996c739573f
Matthew Fosdick <matthew.fosdick@gmail.com>
2018-12-02 19:39:15 -0800

    finally faster than sorted

--- part2/algorithms/sort_then_part.cpp
+++ part2/algorithms/sort_then_part.cpp
@@ -1,6 +1,29 @@
-#include <utility>
 #include <algorithm>
+#include <utility>
 #include "../in_set.h"
+
+template <class RandomIt>
+bool BST(RandomIt first, RandomIt last, int val, InSetAlgorithm::Compare c,
+         RandomIt &ret) {
+	RandomIt too_low = first;
+	RandomIt too_high = last;
+	while((too_low + 1) < too_high) {
+		RandomIt check = too_low + (too_high - too_low) / 2;
+		int cmp = c(val, *check);
+		if(!cmp) {
+			ret = check;
+			return true;
+		}
+		if(cmp > 0) {
+			too_low = check;
+		} else {
+			too_high = check;
+		}
+	}
+	ret = too_high;
+	return false;
+}
+
 bool sortThenPart(std::vector<int> &arr, int val, InSetAlgorithm::Compare cmp) {
 	using std::swap;
 	if(arr.size() < 2) { return false; }
@@ -11,10 +34,10 @@ bool sortThenPart(std::vector<int> &arr, int val, InSetAlgorithm::Compare cmp) {
 	// sort in ascending order
 	std::sort(first, last, [&](auto a, auto b) { return cmp(a, b) <= 0; });
 	// grab partition point
-	auto partition =
-	    std::partition_point(first, last, [&](auto v) { return cmp(v, mid) <= 0; });
+	auto partition = first;
+	BST(first, last, mid, cmp, partition);
 	--last;
-	while(first != last && first != partition) {
+	while(first != last && first != partition && last != (partition - 1)) {
 		auto compare = cmp(*first + *last, val);
 		if(compare == 0) { return true; }
 		if(compare > 0) {
